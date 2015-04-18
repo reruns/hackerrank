7a0d6c6378171f91db3c591a219b419b669fa419
Garrett <graffitisouls@gmail.com>
2015-04-18 19:24:42 -0400

    Power of Large Numbers.

---
+++ number theory/power-of-large-numbers.hs
@@ -0,0 +1,16 @@
+import Control.Monad
+
+--modular exponentiation
+solve :: Integer -> Integer -> Integer
+solve x y = f x y 1 where
+  m = 10^9+7
+  f b 0 r = r
+  f b e r | odd e = f (b*b `mod` m) (e `div` 2) (r*b `mod` m)
+          | otherwise = f (b*b `mod` m) (e `div` 2) r
+
+main = do
+  t <- readLn :: IO Int
+  replicateM_ t $ do
+    cstr <- getLine
+    let [a,b] = map read $ words cstr
+    print $ solve a b 
