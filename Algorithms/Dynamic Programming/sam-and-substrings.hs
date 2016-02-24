94478cd6199aacc2398485a0f705320108209da4
Garrett <graffitisouls@gmail.com>
2016-02-24 16:06:06 -0500

    Sam and Substrings solved.

--- Algorithms/Dynamic Programming/sam-and-substrings.hs
+++ Algorithms/Dynamic Programming/sam-and-substrings.hs
@@ -2,16 +2,19 @@ import System.IO
 
 --Tail recursion is very important.
 solve :: String -> Integer
-solve n = f (reverse n) 0 0 0  where
+solve n = f (reverse n) 0 0 l  where
     f :: String -> Integer -> Integer -> Int -> Integer
     f [] s _ _ = s
     f (n:ns) s ts i = let nt = (ts * 10 + 1) `mod` m
                           num = read [n] in
-                      f ns (s + (num * nt * (toInteger $ l-i)) `mod` m) nt (i+1)     
+                      f ns (s + (num * nt * (toInteger l)) `mod` m) nt (i-1)     
     l = length n
   
 m = 10^9 + 7  
+
 main = do
     h <- openFile "testinput.txt" ReadMode
     n <- hGetContents h
+    --to run test cases on the site, use the next line instead.
+    --n <- getLine
     print $ (solve n) `mod` m
